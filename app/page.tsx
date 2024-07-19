b077864613058aef7b01f39fdc2ac0781df65fdd
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-19 10:37:55 +0200

    Small styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,17 @@ import JoinRoomButton from "./room/joinRoomButton";
 export default async function Home() {
   const publicRooms = await prisma.room.findMany({ where: { public: true } });
   return (
-    <>
-      <h1>Public rooms</h1>
+    <main className="p-5">
+      <h1 className="text-2xl font-bold">Public rooms</h1>
       <ul>
         {publicRooms.map((room) => {
           return (
             <li>
-              <JoinRoomButton room={room} />
+              - <JoinRoomButton room={room} />
             </li>
           );
         })}
       </ul>
-    </>
+    </main>
   );
 }
