b744cd4fb002bb8124078873948f0b674322c138
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-24 14:26:11 +0200

    getting translations

--- app/room/roomView.tsx
+++ app/room/roomView.tsx
@@ -11,11 +11,14 @@ import {
   useTracks,
   useTrackTranscription,
   useLocalParticipant,
+  TrackReferenceOrPlaceholder,
+  useRemoteParticipants,
+  useParticipants,
 } from "@livekit/components-react";
 
 import "@livekit/components-styles";
-import { Track } from "livekit-client";
-import { useEffect, useState } from "react";
+import { RoomEvent, Track } from "livekit-client";
+import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import React from "react";
 
@@ -28,7 +31,6 @@ const RoomView = ({ roomId, userId }: RoomProps) => {
   const router = useRouter();
 
   const [token, setToken] = useState("");
-
   useEffect(() => {
     const fetchToken = async () => {
       try {
@@ -97,20 +99,76 @@ function MyVideoConference() {
   );
 }
 
-function Transcript() {
-  // Initialize transcription to undefined
-  const localParticipant = useLocalParticipant();
-  const { segments } = useTrackTranscription({
-    publication: localParticipant.microphoneTrack,
-    source: Track.Source.Microphone,
-    participant: localParticipant.localParticipant,
+function getTranscript(
+  identity: string
+): TrackReferenceOrPlaceholder | undefined {
+  const tracks = useTracks();
+  const participants = useParticipants({
+    updateOnlyOn: [RoomEvent.ParticipantMetadataChanged],
   });
+  const participant = participants.find((p) => p.identity === identity);
+  if (!participant) {
+    return undefined;
+  }
+
+  let participantTrack: TrackReferenceOrPlaceholder | undefined;
+  const aat = tracks.find(
+    (trackRef) =>
+      trackRef.publication.kind === Track.Kind.Audio &&
+      trackRef.participant.identity === identity
+  );
+  if (aat) {
+    participantTrack = aat;
+  } else if (participant) {
+    participantTrack = {
+      participant: participant,
+      source: Track.Source.Microphone,
+    };
+  }
+  return participantTrack;
+}
+
+function TranscriptionTile({ identity }: { identity: string }) {
+  const audioTrack = getTranscript(identity);
+  if (!audioTrack) {
+    return <></>;
+  } else {
+    return <ActualTranscription audioTrack={audioTrack} />;
+  }
+}
+
+function ActualTranscription({
+  audioTrack,
+}: {
+  audioTrack: TrackReferenceOrPlaceholder;
+}) {
+  const { segments } = useTrackTranscription(audioTrack);
 
   if (segments && segments.length > 0) {
     return <h1>{segments.at(-1)?.text}</h1>;
   } else {
     return <h1>No transcription</h1>;
   }
+}
+function Transcript() {
+  const participants = useParticipants();
+
+  return (
+    <div className="flex flex-col gap-16 h-full ">
+      <div className="flex-1">
+        <p>
+          {participants.map((participant) => {
+            return (
+              <>
+                <h1>{participant.identity}:</h1>
+                <TranscriptionTile identity={participant.identity} />
+              </>
+            );
+          })}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default RoomView;
