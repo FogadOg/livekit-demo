474ad771cea0d1eaaf6f9910696773b67b0292c3
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-25 15:28:22 +0200

    Simplified showing of segments

--- app/room/transcription/transcriptionUtil/transcription.tsx
+++ app/room/transcription/transcriptionUtil/transcription.tsx
@@ -12,31 +12,16 @@ export const Transcription = ({
 }: {
   audioTrack: TrackReferenceOrPlaceholder;
 }) => {
-  const [segmentsDict, setSegmentsDict] = useState<{ [key: string]: string }>(
-    {}
-  );
-
   const { segments } = useTrackTranscription(audioTrack);
 
-  useEffect(() => {
-    if (segments) {
-      setSegmentsDict((prevSegmentsDict) => {
-        const updatedDict = { ...prevSegmentsDict };
-        updatedDict[audioTrack.participant.identity] = "";
-
-        segments.forEach((segment) => {
-          updatedDict[audioTrack.participant.identity] += segment.text + " ";
-        });
-
-        return updatedDict;
-      });
-    }
-  }, [segments, audioTrack.participant.identity]);
-
-  const latestSegmentKey = Object.keys(segmentsDict).pop();
-  const latestSegmentText = latestSegmentKey
-    ? segmentsDict[latestSegmentKey]
-    : "No transcription available";
-
-  return <p>{latestSegmentText}</p>;
+  if (segments.length > 0) {
+    return (
+      <p>
+        {segments.map((segment) => {
+          return <>{segment.text + " "}</>;
+        })}
+      </p>
+    );
+  }
+  return <p>No transcription available</p>;
 };
