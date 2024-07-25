25a969cbebc157799135a0f107b03584246cfc0b
fogadOg <fogadm06@gmail.com>
2024-07-25 12:52:13 +0200

    adding spacing

--- app/room/transcription/transcriptionUtil/getTranscription.ts
+++ app/room/transcription/transcriptionUtil/getTranscription.ts
@@ -10,7 +10,7 @@ import {
   import React from "react";
 
 
-export const GetTranscript = (
+export const GetTranscript=(
     identity: string
   ): TrackReferenceOrPlaceholder | undefined => {
     const tracks = useTracks();
