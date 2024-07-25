9576dba80beeb3fda0dd50b57ea56cf237576a64
fogadOg <fogadm06@gmail.com>
2024-07-25 08:35:34 +0200

    adding code for caption

--- app/room/transcription/caption.tsx
+++ app/room/transcription/caption.tsx
@@ -1,11 +1,27 @@
-
-
-
-
-
-
-
-
-export const caption = () => {
-}
+import {TranscriptionTile} from "./transcriptionUtil/transcription"
+import {
+    useParticipants,
+  } from "@livekit/components-react";
   
+const Caption = () => {
+    const participants = useParticipants();
+  
+    return (
+      <div className="flex flex-col gap-16 h-full ">
+        <div className="flex-1">
+          <p>
+            {participants.map((participant) => {
+              return (
+                <>
+                  <h1>{participant.identity}:</h1>
+                  <TranscriptionTile identity={participant.identity} />
+                </>
+              );
+            })}
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Caption;
