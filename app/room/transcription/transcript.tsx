88119d4f4c62c619ea93425925104d088dc60de4
fogadOg <fogadm06@gmail.com>
2024-07-25 12:55:47 +0200

    code to transcipt

--- app/room/transcription/transcript.tsx
+++ app/room/transcription/transcript.tsx
@@ -1,14 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { TranscriptionTile } from "./transcriptionUtil/transcriptionTile";
-import { useParticipants, useTrackTranscription } from "@livekit/components-react";
-import { GetTranscript } from "./transcriptionUtil/getTranscription";
-
-
-
+import {TranscriptionTile} from "./transcriptionUtil/transcriptionTile"
+import {
+    useParticipants,
+  } from "@livekit/components-react";
   
 export const Transcript = () => {
- 
-    return (<></>);
-    
-};
+    const participants = useParticipants();
+  
+    return (
+      <div className="flex flex-col gap-16 h-full ">
+        <div className="flex-1">
+            {participants.map((participant) => {
+              return (
+                <div key={participant.identity}>
+                  <h3>{participant.identity}:</h3>
+                  <TranscriptionTile identity={participant.identity} />
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    );
+  }
   
