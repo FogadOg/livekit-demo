2e1d17e32404e300bf6e75a75af1f3b9495329e6
fogadOg <fogadm06@gmail.com>
2024-07-26 11:00:38 +0200

    removing navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "./component/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="business">
       <body className={inter.className}>
-        <Navbar/>
         {children}
       </body>
     </html>
