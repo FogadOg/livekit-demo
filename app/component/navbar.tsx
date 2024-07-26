6d51aa694fcb25c99d5f5c6ccd75fe36013da522
fogadOg <fogadm06@gmail.com>
2024-07-26 10:37:33 +0200

    adding navbar

---
+++ app/component/navbar.tsx
@@ -0,0 +1,29 @@
+
+
+
+export const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 px-10 py-5">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">Livekit Demo</a>
+            </div>
+            <div className="flex-none">
+                <button className="btn btn-square btn-ghost">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="inline-block h-5 w-5 stroke-current">
+                    <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+                </svg>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+
