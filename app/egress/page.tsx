b636231a0f4d93f45c4daa487f6ddf8052e782d8
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-29 15:51:48 +0200

    Css for recording

--- app/egress/page.tsx
+++ app/egress/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import "@livekit/components-styles";
 import "@livekit/components-styles/prefabs";
 import EgressHelper from "@livekit/egress-sdk";
 import RoomPage from "./egressRoom";
 
 export default function Page() {
+  useEffect(() => {
+    // Add class to the body
+    document.body.classList.add("egress-body");
+
+    // Cleanup function to remove class
+    return () => {
+      document.body.classList.remove("egress-body");
+    };
+  }, []);
+
   return (
     <div className="container">
       <RoomPage
