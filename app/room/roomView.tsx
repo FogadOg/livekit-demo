70904e4848f149611a00b47926365a6b66b364a1
fogadOg <fogadm06@gmail.com>
2024-07-29 11:12:33 +0200

    moving video conferance to own file

--- app/room/roomView.tsx
+++ app/room/roomView.tsx
@@ -5,18 +5,14 @@ import { useRouter } from "next/navigation";
 import {
   LayoutContextProvider,
   Chat,
-  GridLayout,
   LiveKitRoom,
-  ParticipantTile,
   RoomAudioRenderer,
-  useTracks,
 } from "@livekit/components-react";
-import { Track } from "livekit-client";
 import "@livekit/components-styles";
 
-import Caption from "./transcription/caption";
 import { CustomControlBar } from "../component/customControlBar";
 import { deleteRoomIfEmpty } from "../actions/roomActions";
+import { VideoConference } from "./videoConference";
 
 interface RoomProps {
   roomId: string;
@@ -78,7 +74,7 @@ const RoomView = ({ roomId, userId }: RoomProps) => {
         }}
       >
         <div className="flex">
-          <MyVideoConference />
+          <VideoConference/>
           <Chat />
         </div>
         <RoomAudioRenderer />
@@ -88,31 +84,5 @@ const RoomView = ({ roomId, userId }: RoomProps) => {
   );
 };
 
-const MyVideoConference = () => {
-  const tracks = useTracks(
-    [
-      { source: Track.Source.Camera, withPlaceholder: true },
-      { source: Track.Source.ScreenShare, withPlaceholder: false },
-    ],
-    { onlySubscribed: false }
-  );
-
-  const filteredTracks = tracks.filter((track) => !track.participant.isAgent);
-  const agentPresent = filteredTracks.length !== tracks.length;
-
-  return (
-    <GridLayout
-      tracks={filteredTracks}
-      style={{ height: "calc(100vh - var(--lk-control-bar-height))" }}
-    >
-      <div className="relative">
-        <ParticipantTile className="h-full" />
-        <div className="absolute top-[75%] origin-top left-[2%] max-w-[96%] xl:top-[80%] xl:left-[20%] xl:max-w-[65%]">
-          <Caption agentPresent={agentPresent} />
-        </div>
-      </div>
-    </GridLayout>
-  );
-};
 
 export default RoomView;
