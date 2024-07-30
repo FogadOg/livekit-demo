6a3197d70eeffe5bf622b46d46e4c2925aa75b29
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-30 13:10:01 +0200

    Every room is active, room aren't active is deleted

--- app/room/components/room/joinPublicRoom.tsx
+++ app/room/components/room/joinPublicRoom.tsx
@@ -17,13 +17,7 @@ const JoinPublicRoom = ({ room, participantsCount }: JoinPublicRoomProps) => {
   return (
     <div key={room.id} className="card bg-base-100 w-64 shadow-md border ">
       <div className="card-body">
-        <div className="flex items-center gap-2">
-          <h2 className="card-title">{room.name}</h2>
-
-          {participantsCount > 0 && (
-            <div className="badge badge-secondary">Active</div>
-          )}
-        </div>
+        <h2 className="card-title">{room.name}</h2>
         <p>Participants count: {participantsCount}</p>
         <div className="card-actions justify-end">
           <button onClick={handleClick} className="btn btn-primary">
