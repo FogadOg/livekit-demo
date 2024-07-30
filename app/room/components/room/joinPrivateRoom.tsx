8ca28015605d3a1d47d874894d3bd92affc1ae53
fogadOg <fogadm06@gmail.com>
2024-07-30 13:10:56 +0200

    switching from error message to alert

--- app/room/components/room/joinPrivateRoom.tsx
+++ app/room/components/room/joinPrivateRoom.tsx
@@ -9,18 +9,16 @@ interface JoinPrivateRoomProps {
 
 const JoinPrivateRoom = ({ privateRoomIds }: JoinPrivateRoomProps) => {
   const router = useRouter();
-  const [error, setError] = useState("");
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // don't refresh
     const formData = new FormData(event.currentTarget);
 
     let roomId = (formData.get("roomId") as string | null) || "default name";
 
-    setError("");
     if (privateRoomIds.includes(roomId)) {
       router.push(`/room?roomId=${roomId}`);
     } else {
-      setError("Room doesn't exist");
+      alert("Room doesn't exist");
     }
   };
 
@@ -41,7 +39,6 @@ const JoinPrivateRoom = ({ privateRoomIds }: JoinPrivateRoomProps) => {
         <div>
           <input type="submit" value="Join" className="btn btn-primary" />
         </div>
-        <p className="text-red-500">{error}</p>
       </form>
     </div>
   );
