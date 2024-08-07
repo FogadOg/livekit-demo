ebb0e0f36aeb242a7614731f833e6ff88d8b65b5
fogadOg <fogadm06@gmail.com>
2024-08-07 08:58:26 +0200

    fixing admin panel

--- app/components/adminControls.tsx
+++ app/components/adminControls.tsx
@@ -1,5 +1,5 @@
 import { useParticipants, useRoomInfo } from "@livekit/components-react";
-import { Participant } from "livekit-client";
+import { Participant, Track } from "livekit-client";
 import { useState } from "react";
 import useIsAdmin from "../hooks/useIsAdmin";
 import {
@@ -27,33 +27,48 @@ export function AdminControls({ token }: { token: string }) {
   if (!isAdmin) {
     return <></>;
   }
+  // console.log(p.permissions?.canPublishSources.includes(TrackSource.MICROPHONE));
+  
   return (
-    <div className="relative">
-          <ol className="bottom-5  ">
-            {participants.map((p) => (
-              <li key={p.sid} className="flex gap-2">
-                <p className="pe-3">{p.identity}</p>
+    <div role="tablist" className="tabs tabs-bordered">
+
+      {participants.map((p) => (
+          <>
+            <input type="radio" name="my_tabs_1" role="tab" className="tab text-ellipsis" aria-label={p.identity} />
+            <div role="tabpanel" className="tab-content p-10 grid">
+              <div className={p.permissions?.canPublishSources.includes(TrackSource.MICROPHONE) ? "btn btn-success" : "btn btn-error"}>
                 <ToggleTrackSource
                   trackSource={TrackSource.MICROPHONE}
                   p={p}
                   updateTrackSources={updateTrackSources}
                 />
+              </div>
+
+              <div className={p.permissions?.canPublishSources.includes(TrackSource.CAMERA) ? "btn btn-success" : "btn btn-error"}>
                 <ToggleTrackSource
                   trackSource={TrackSource.CAMERA}
                   p={p}
                   updateTrackSources={updateTrackSources}
                 />
+
+              </div>
+
+              <div className={p.permissions?.canPublishSources.includes(TrackSource.SCREEN_SHARE) ? "btn btn-success" : "btn btn-error"}>
                 <ToggleTrackSource
                   trackSource={TrackSource.SCREEN_SHARE}
                   p={p}
                   updateTrackSources={updateTrackSources}
                 />
+              </div>
+
+              <div className={p.permissions?.canPublishSources.includes(TrackSource.SCREEN_SHARE_AUDIO) ? "btn btn-success" : "btn btn-error"}>
                 <ToggleTrackSource
                   trackSource={TrackSource.SCREEN_SHARE_AUDIO}
                   p={p}
                   updateTrackSources={updateTrackSources}
                 />
-                <p className="">
+              </div>
+
                   {/*TODO User not being fully kicked*/}
                   <button
                     onClick={async () => {
@@ -64,20 +79,19 @@ export function AdminControls({ token }: { token: string }) {
                   >
                     Toggle messaging
                   </button>
-                </p>
-                <p className="">
                   {/*TODO User not being fully kicked*/}
                   <button
+                    className="btn btn-outline btn-accent mt-10"
                     onClick={async () => {
                       await kickParticipant(p.identity, token);
                     }}
                   >
                     Kick
                   </button>
-                </p>
-              </li>
-            ))}
-          </ol>
+            </div>
+
+          </>
+      ))}
     </div>
   );
 }
