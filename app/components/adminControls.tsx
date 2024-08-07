9fcd9b320d1299c92dd2552c1bc81a7065ac276b
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-07 08:41:08 +0200

    Typo

--- app/components/adminControls.tsx
+++ app/components/adminControls.tsx
@@ -9,7 +9,7 @@ import {
 import { TrackSource } from "livekit-server-sdk";
 import { ToggleTrackSource } from "./toggleTrackSource";
 
-import { ControlIcon } from "../assets/conrtolIcon";
+import { ControlIcon } from "../assets/controlIcon";
 
 export function AdminControls({ token }: { token: string }) {
   const isAdmin = useIsAdmin(token);
@@ -88,7 +88,7 @@ export function AdminControls({ token }: { token: string }) {
         className="btn lk-button"
         onClick={() => setOpen((cur) => !cur)}
       >
-        <ControlIcon/>
+        <ControlIcon />
         AdminControls
       </button>
     </div>
