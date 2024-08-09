c6a89905c42d72c1a57c2b292b6f419b7ec14b20
fogadOg <fogadm06@gmail.com>
2024-08-09 09:59:46 +0200

    function to set metadata

--- app/actions/roomMetadata.ts
+++ app/actions/roomMetadata.ts
@@ -1,13 +1,28 @@
+"use server"
+
 import roomService from "@/lib/roomService"
+import { json } from "stream/consumers";
+
 
 
 
 export const getRoomMetadata = async (roomName: string) => {
   const room = (await roomService.listRooms([roomName]))[0]
 
-  return room.metadata
+  return JSON.parse(room.metadata)
 }
 
+export const setRoomMetadata = async (roomName: string, fieldName: string, newData: any) => {
+    const metadata = await getRoomMetadata(roomName)
+    let metadataObject
+    if(metadata == "") {
+        metadataObject = {}
+    } else {
+        metadataObject = metadata
+    }
+    metadataObject[fieldName] = newData
 
-
-
+    roomService.updateRoomMetadata(roomName, JSON.stringify(newData))
+    console.log("getRoomMetadata: ",getRoomMetadata(roomName));
+    
+}
