e7b66aa8dec6cb35fcae333f99cf8cd97c71244f
fogadOg <fogadm06@gmail.com>
2024-08-12 14:52:04 +0200

    encryption function

---
+++ app/actions/sha256.ts
@@ -0,0 +1,12 @@
+async function sha256(message: string) {
+    if(message !== null) {
+      const msgBuffer = new TextEncoder().encode(message);                    
+      const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+      const hashArray = Array.from(new Uint8Array(hashBuffer));
+      const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+      return hashHex
+    }
+
+    return message
+}
