c30fad8ca4919d0925880765a6657c6c4732a962
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-12 11:37:08 +0200

    Understanble name

--- app/actions/roomMetadata.ts
+++ app/actions/roomMetadata.ts
@@ -1,29 +1,27 @@
-"use server"
+"use server";
 
-import roomService from "@/lib/roomService"
-import { json } from "stream/consumers";
-
-
-
+import roomService from "@/lib/roomService";
 
 export const getRoomMetadata = async (roomName: string) => {
-  const room = (await roomService.listRooms([roomName]))[0]
+  const room = (await roomService.listRooms([roomName]))[0];
 
-  return room.metadata
-}
+  return room.metadata;
+};
 
-export const setRoomMetadata = async (roomName: string, fieldName: string, newData: any) => {
-    const metadata = await getRoomMetadata(roomName)
-    
-    let metadataObject
-    if(metadata == "") {
-        metadataObject = {}
-    } else {
-        metadataObject = JSON.parse(metadata)
-    }
-    metadataObject[fieldName] = newData
-    
+export const addMetadataToRoom = async (
+  roomName: string,
+  fieldName: string,
+  newData: any
+) => {
+  const metadata = await getRoomMetadata(roomName);
 
-    roomService.updateRoomMetadata(roomName, JSON.stringify(metadataObject))
-    
-}
+  let metadataObject;
+  if (metadata == "") {
+    metadataObject = {};
+  } else {
+    metadataObject = JSON.parse(metadata);
+  }
+  metadataObject[fieldName] = newData;
+
+  roomService.updateRoomMetadata(roomName, JSON.stringify(metadataObject));
+};
--- app/actions/transcription.ts
+++ app/actions/transcription.ts
@@ -2,36 +2,34 @@
 
 import prisma from "../../lib/prisma";
 import roomService from "@/lib/roomService";
-import { getRoomMetadata, setRoomMetadata } from "./roomMetadata";
+import { getRoomMetadata, addMetadataToRoom } from "./roomMetadata";
 
 export async function appendTranscription(
   username: string,
   roomName: string,
   transcription: string
-) {  
-  const room = (await roomService.listRooms([roomName]))[0]
-  let metadata
-  
-  if (room.metadata.length == 0){
-    metadata = {}
-  }else {
-    metadata = JSON.parse(JSON.parse(room.metadata)["transcript"])  
-  }
-  
-  try{
-    metadata[username] = metadata[username] +transcription
-  }catch{
-    metadata[username] = transcription
+) {
+  const room = (await roomService.listRooms([roomName]))[0];
+  let metadata;
+
+  if (room.metadata.length == 0) {
+    metadata = {};
+  } else {
+    metadata = JSON.parse(JSON.parse(room.metadata)["transcript"]);
   }
 
-  setRoomMetadata(roomName, "transcript", JSON.stringify(metadata))
+  try {
+    metadata[username] = metadata[username] + transcription;
+  } catch {
+    metadata[username] = transcription;
+  }
 
+  addMetadataToRoom(roomName, "transcript", JSON.stringify(metadata));
 }
 
 export const GetTranscription = async (roomName: string) => {
-  const metadata = (await roomService.listRooms([roomName]))[0].metadata
-  const transcriptions = JSON.parse(metadata)["transcript"]
-  
+  const metadata = (await roomService.listRooms([roomName]))[0].metadata;
+  const transcriptions = JSON.parse(metadata)["transcript"];
 
   return JSON.parse(transcriptions);
 };
