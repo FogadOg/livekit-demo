bae9ee3f91b3814772e18ef72d7eb01251f25788
fogadOg <fogadm06@gmail.com>
2024-08-12 11:06:54 +0200

    adding conditional rendering

--- app/components/admin/adminControls/permissionControls.tsx
+++ app/components/admin/adminControls/permissionControls.tsx
@@ -15,28 +15,33 @@ import {
 } from "../../../actions/userActions";
 import { TrackSource } from "livekit-server-sdk";
 import { ToggleTrackSource } from "./toggleTrackSource";
-import { Modal } from "../../../components/modal"
-import PermissionPanel from "./permissionPanel"
+import { Modal } from "../../../components/modal";
+import PermissionPanel from "./permissionPanel";
 
-// TODO no users to contrll
 export function PermissionControls({ token }: { token: string }) {
   const isAdmin = useIsAdmin(token);
   const identity = useLocalParticipant().localParticipant.identity;
   let participants = useParticipants();
+
   participants = participants.filter(
     (p) => !p.isAgent && p.identity !== identity
   );
 
   return (
     <div className="flex flex-wrap">
-      {participants.map((p) => (
-        <Modal
-          title={`${p.identity} permissions`}
-          content={<PermissionPanel p={p} token={token} />}
-          buttonText={`${p.identity} permissions`}
-          modelName={`user ${p.identity} permissions`}
-        />
-      ))}
+      {participants.length === 0 ? (
+        <p>No users available</p>
+      ) : (
+        participants.map((p) => (
+          <Modal
+            key={p.identity}
+            title={`${p.identity} permissions`}
+            content={<PermissionPanel p={p} token={token} />}
+            buttonText={`${p.identity} permissions`}
+            modelName={`user ${p.identity} permissions`}
+          />
+        ))
+      )}
     </div>
   );
 }
