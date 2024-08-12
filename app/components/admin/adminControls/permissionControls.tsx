6f0d6c1f0ce825ad51806d4f8e4ce156528f42cf
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-12 13:44:40 +0200

    Permissions icon

--- app/components/admin/adminControls/permissionControls.tsx
+++ app/components/admin/adminControls/permissionControls.tsx
@@ -4,6 +4,7 @@ import {
 } from "@livekit/components-react";
 import { Modal } from "../../../components/modal";
 import PermissionPanel from "./permissionPanel";
+import { PermissionIcon } from "@/app/assets/permissionsIcon";
 
 export function PermissionControls({ token }: { token: string }) {
   const identity = useLocalParticipant().localParticipant.identity;
@@ -23,8 +24,9 @@ export function PermissionControls({ token }: { token: string }) {
             key={p.identity}
             title={`${p.identity} permissions`}
             content={<PermissionPanel p={p} token={token} />}
-            buttonText={`${p.identity} permissions`}
+            buttonText={`${p.identity}'s permissions`}
             modelName={`user ${p.identity} permissions`}
+            icon={<PermissionIcon />}
           />
         ))
       )}
