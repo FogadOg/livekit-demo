7983d3f9ae2d02b2d9007c3612202a9006361f8b
fogadOg <fogadm06@gmail.com>
2024-08-12 11:07:34 +0200

    removing unused imports

--- app/components/admin/adminControls/permissionControls.tsx
+++ app/components/admin/adminControls/permissionControls.tsx
@@ -1,25 +1,11 @@
 import {
   useLocalParticipant,
   useParticipants,
-  useRoomInfo,
 } from "@livekit/components-react";
-import {
-  LocalParticipant,
-  Participant,
-  RemoteParticipant,
-} from "livekit-client";
-import useIsAdmin from "../../../hooks/useIsAdmin";
-import {
-  kickParticipant,
-  updateParticipantPermissions,
-} from "../../../actions/userActions";
-import { TrackSource } from "livekit-server-sdk";
-import { ToggleTrackSource } from "./toggleTrackSource";
 import { Modal } from "../../../components/modal";
 import PermissionPanel from "./permissionPanel";
 
 export function PermissionControls({ token }: { token: string }) {
-  const isAdmin = useIsAdmin(token);
   const identity = useLocalParticipant().localParticipant.identity;
   let participants = useParticipants();
 
