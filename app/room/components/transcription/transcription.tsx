60f7e81d2ad02f049028d0d9f5f280c7a38492bc
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-13 10:39:09 +0200

    Checking for old captions every 3000 seconds

--- app/room/components/transcription/transcription.tsx
+++ app/room/components/transcription/transcription.tsx
@@ -22,6 +22,7 @@ export const Transcription = ({
   const { segments } = useTrackTranscription(audioTrack);
   //Saving only if new entry
   const [savedIndex, setSavedIndex] = useState(0);
+  const [hidden, setHidden] = useState(false);
 
   const roomInfo = useRoomInfo();
   useEffect(() => {
@@ -32,19 +33,38 @@ export const Transcription = ({
         segments.at(-1)?.final &&
         segments.length > savedIndex
       ) {
+        setHidden(false);
         appendTranscription(
           audioTrack.participant.identity,
           roomInfo.name,
           segments.at(-1)?.text! + " "
         );
-        
+
         setSavedIndex(segments.length);
       }
     }
-    
-  }, [audioTrack, segments, savedIndex])
-  
+  }, [audioTrack, segments, savedIndex]);
 
+  const checkForOutdatedCaption = () => {
+    if (segments && segments.length > 0) {
+      const receivedAt = segments.at(-1)?.receivedAt!;
+
+      const currentTime = Date.now();
+      const elapsedTime = currentTime - receivedAt;
+      if (elapsedTime > 1000) {
+        setHidden(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (hidden) {
+      return;
+    }
+
+    const interval = setInterval(checkForOutdatedCaption, 3000);
+    return () => clearInterval(interval);
+  }, [segments, hidden]);
 
   if (segments.length > 0 && !onlyLastSegment) {
     return (
@@ -56,9 +76,10 @@ export const Transcription = ({
     );
   }
 
-  if (onlyLastSegment && segments && segments.length > 0) {
+  if (onlyLastSegment && segments && segments.length > 0 && !hidden) {
     return <p className="bg-[rgba(0,0,0,0.5)] p-2">{segments.at(-1)?.text}</p>;
   }
+
   if (onlyLastSegment) {
     return <p></p>;
   }
