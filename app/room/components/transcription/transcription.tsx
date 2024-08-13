fc6d4a81a46bfe94cd2994bc5c32173d96e3fa3b
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-13 14:04:06 +0200

    Recived timestamp not accurate on not local

--- app/room/components/transcription/transcription.tsx
+++ app/room/components/transcription/transcription.tsx
@@ -13,16 +13,12 @@ import { appendTranscription } from "@/app/actions/transcription";
 
 export const Transcription = ({
   audioTrack,
-  onlyLastSegment = false,
 }: {
   audioTrack: TrackReferenceOrPlaceholder;
-
-  onlyLastSegment?: boolean;
 }) => {
   const { segments } = useTrackTranscription(audioTrack);
   //Saving only if new entry
   const [savedIndex, setSavedIndex] = useState(0);
-  const [hidden, setHidden] = useState(false);
 
   const roomInfo = useRoomInfo();
   useEffect(() => {
@@ -33,7 +29,6 @@ export const Transcription = ({
         segments.at(-1)?.final &&
         segments.length > savedIndex
       ) {
-        setHidden(false);
         appendTranscription(
           audioTrack.participant.identity,
           roomInfo.name,
@@ -45,43 +40,9 @@ export const Transcription = ({
     }
   }, [audioTrack, segments, savedIndex]);
 
-  const checkForOutdatedCaption = () => {
-    if (segments && segments.length > 0) {
-      const receivedAt = segments.at(-1)?.receivedAt!;
-
-      const currentTime = Date.now();
-      const elapsedTime = currentTime - receivedAt;
-      if (elapsedTime > 1000) {
-        setHidden(true);
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (hidden) {
-      return;
-    }
-
-    const interval = setInterval(checkForOutdatedCaption, 3000);
-    return () => clearInterval(interval);
-  }, [segments, hidden]);
-
-  if (segments.length > 0 && !onlyLastSegment) {
-    return (
-      <p>
-        {segments.map((segment) => {
-          return <span key={segment.id}>{segment.text + " "}</span>;
-        })}
-      </p>
-    );
-  }
-
-  if (onlyLastSegment && segments && segments.length > 0 && !hidden) {
+  if (segments && segments.length > 0) {
     return <p className="bg-[rgba(0,0,0,0.5)] p-2">{segments.at(-1)?.text}</p>;
   }
 
-  if (onlyLastSegment) {
-    return <p></p>;
-  }
-  return <p>User not spoken</p>;
+  return <p></p>;
 };
