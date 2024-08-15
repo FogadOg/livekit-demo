fad14d0f8413d77305e33f5d2b1f229b452850dd
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-15 15:16:07 +0200

    Cleaned code

--- app/components/admin/adminControls/cameraManagement.tsx
+++ app/components/admin/adminControls/cameraManagement.tsx
@@ -1,57 +1,30 @@
 import {
-  isTrackReference,
-  ParticipantContext,
-  TrackRefContext,
-  TrackReference,
-  useMaybeTrackRefContext,
   useParticipants,
   useRoomInfo,
-  useTrackByName,
+  useTracks,
   VideoTrack,
 } from "@livekit/components-react";
-import {
-  ParticipantKind,
-  RemoteVideoTrack,
-  Track,
-  TrackPublication,
-} from "livekit-client";
+import { ParticipantKind, Track } from "livekit-client";
 import RoomIngress from "../roomIngress";
 import { changeMetaDataToParticipant } from "@/app/actions/metadataAction";
 
 export const CameraManagement = ({ token }: { token: string }) => {
-  const cameras = useParticipants().filter(
-    (p) => p.kind === ParticipantKind.INGRESS
+  const cameras = useTracks([Track.Source.Camera]).filter(
+    (track) => track.participant.kind === ParticipantKind.INGRESS
   );
+
   const room = useRoomInfo();
   return (
     <>
       <form className="mb-10 menu">
-        {cameras.map((camera, index) => {
-          let videoRef = undefined;
-
-          let audioRef = undefined;
-          camera.videoTrackPublications.forEach((value, key) => {
-            console.log(key);
-            console.log(value);
-            if (value.track) {
-              if (value.kind == Track.Kind.Video) {
-                videoRef = {
-                  participant: camera,
-                  publication: value,
-                  source: value.track.source,
-                };
-              }
-            }
-          });
-
-          console.log(audioRef);
+        {cameras.map((videoRef, index) => {
           return (
             <li key={"camera-" + index}>
               <label
                 className="flex justify-between items-center first-letter:capitalize"
                 htmlFor={index.toString()}
               >
-                {camera.name || camera.identity}
+                {videoRef.participant.name || videoRef.participant.identity}
 
                 {videoRef && <VideoTrack trackRef={videoRef} />}
 
@@ -60,12 +33,12 @@ export const CameraManagement = ({ token }: { token: string }) => {
                   name={index.toString()}
                   className="toggle toggle-success"
                   id={index.toString()}
-                  checked={camera.metadata?.includes("Active")}
+                  checked={videoRef.participant.metadata?.includes("Active")}
                   onChange={async () => {
                     changeMetaDataToParticipant(
                       room.name,
-                      camera.identity,
-                      camera.metadata?.includes("Active")
+                      videoRef.participant.identity,
+                      videoRef.participant.metadata?.includes("Active")
                         ? "Inactive"
                         : "Active"
                     );
