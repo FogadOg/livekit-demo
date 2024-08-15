ff65c2e9f34704701a26495bfcaa5620f8a5efe5
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-15 14:07:18 +0200

    Showing video while choosing cmaera

--- app/components/admin/adminControls/cameraManagement.tsx
+++ app/components/admin/adminControls/cameraManagement.tsx
@@ -1,21 +1,22 @@
 import {
-  generatePermissionToken,
-  updateParticipantPermissions,
-} from "@/app/actions/adminActions";
-import {
+  isTrackReference,
+  ParticipantContext,
+  TrackRefContext,
+  TrackReference,
+  useMaybeTrackRefContext,
   useParticipants,
-  useRoomContext,
   useRoomInfo,
+  useTrackByName,
+  VideoTrack,
 } from "@livekit/components-react";
-import { ParticipantKind } from "livekit-client";
-import { TrackSource, VideoGrant } from "livekit-server-sdk";
-import { useState, useEffect } from "react";
+import {
+  ParticipantKind,
+  RemoteVideoTrack,
+  Track,
+  TrackPublication,
+} from "livekit-client";
 import RoomIngress from "../roomIngress";
 import { changeMetaDataToParticipant } from "@/app/actions/metadataAction";
-
-const trackSourceValues = Object.values(TrackSource).filter(
-  (value) => typeof value !== "number" && value !== "UNKNOWN"
-);
 
 export const CameraManagement = ({ token }: { token: string }) => {
   const cameras = useParticipants().filter(
@@ -25,31 +26,55 @@ export const CameraManagement = ({ token }: { token: string }) => {
   return (
     <>
       <form className="mb-10 menu">
-        {cameras.map((camera, index) => (
-          <li key={"camera-" + index}>
-            <label
-              className="flex justify-between items-center first-letter:capitalize"
-              htmlFor={index.toString()}
-            >
-              {camera.name || camera.identity}
+        {cameras.map((camera, index) => {
+          let videoRef = undefined;
 
-              <input
-                type="checkbox"
-                name={index.toString()}
-                className="toggle toggle-success"
-                id={index.toString()}
-                checked={camera.metadata?.includes("Active")}
-                onChange={async () => {
-                  changeMetaDataToParticipant(
-                    room.name,
-                    camera.identity,
-                    camera.metadata?.includes("Active") ? "Inactive" : "Active"
-                  );
-                }}
-              />
-            </label>
-          </li>
-        ))}
+          let audioRef = undefined;
+          camera.videoTrackPublications.forEach((value, key) => {
+            console.log(key);
+            console.log(value);
+            if (value.track) {
+              if (value.kind == Track.Kind.Video) {
+                videoRef = {
+                  participant: camera,
+                  publication: value,
+                  source: value.track.source,
+                };
+              }
+            }
+          });
+
+          console.log(audioRef);
+          return (
+            <li key={"camera-" + index}>
+              <label
+                className="flex justify-between items-center first-letter:capitalize"
+                htmlFor={index.toString()}
+              >
+                {camera.name || camera.identity}
+
+                {videoRef && <VideoTrack trackRef={videoRef} />}
+
+                <input
+                  type="checkbox"
+                  name={index.toString()}
+                  className="toggle toggle-success"
+                  id={index.toString()}
+                  checked={camera.metadata?.includes("Active")}
+                  onChange={async () => {
+                    changeMetaDataToParticipant(
+                      room.name,
+                      camera.identity,
+                      camera.metadata?.includes("Active")
+                        ? "Inactive"
+                        : "Active"
+                    );
+                  }}
+                />
+              </label>
+            </li>
+          );
+        })}
       </form>
 
       <RoomIngress roomName={room.name} adminToken={token} />
