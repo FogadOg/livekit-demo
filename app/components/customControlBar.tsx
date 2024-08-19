13fbf0ca3bd65738c7cfdb6a9f689b1148600243
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-19 11:50:27 +0200

    Abel to send messages

--- app/components/customControlBar.tsx
+++ app/components/customControlBar.tsx
@@ -1,7 +1,9 @@
 import {
   ControlBar,
   ControlBarProps,
+  useLocalParticipant,
   useLocalParticipantPermissions,
+  useRoomContext,
 } from "@livekit/components-react";
 
 import { TrackSource } from "livekit-server-sdk";
@@ -9,17 +11,29 @@ import useIsAdmin from "@/app/hooks/useIsAdmin";
 import { AdminControls } from "./admin/adminControls";
 import { Modal } from "./modal";
 import { PauseButton } from "./admin/adminControls/pauseButton";
+import { Participant, RemoteParticipant, RoomEvent } from "livekit-client";
 
 interface CustomControlBarProps extends ControlBarProps {
   token: string;
 }
 
 export function CustomControlBar({ token, ...props }: CustomControlBarProps) {
+  const localParticipant = useLocalParticipant();
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+  const room = useRoomContext();
+
   const permissions = useLocalParticipantPermissions();
   const isAdmin = useIsAdmin(token);
+
+  room.on(RoomEvent.DataReceived, (payload, participant, kind) => {
+    const strData = decoder.decode(payload);
+    console.log(`Got message from ${participant?.identity}`);
+    console.log(`Message is ${strData}`);
+  });
   return (
     <div className="lk-control-bar">
-      {isAdmin && 
+      {isAdmin && (
         <>
           <Modal
             title="Admin panel"
@@ -27,9 +41,9 @@ export function CustomControlBar({ token, ...props }: CustomControlBarProps) {
             buttonText="View admin panel"
             modelName="adminPanel"
           />
-          <PauseButton/>
+          <PauseButton />
         </>
-    }
+      )}
       <ControlBar
         controls={{
           camera: permissions?.canPublishSources.includes(TrackSource.CAMERA),
@@ -42,6 +56,16 @@ export function CustomControlBar({ token, ...props }: CustomControlBarProps) {
         }}
         {...props}
       />
+      <button
+        onClick={() => {
+          localParticipant.localParticipant.publishData(
+            encoder.encode("Hello"),
+            { reliable: true, topic: "EmojiReaction" }
+          );
+        }}
+      >
+        Emoji React
+      </button>
     </div>
   );
 }
