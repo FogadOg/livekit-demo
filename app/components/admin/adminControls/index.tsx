8d593ed1a8cdbf375cb4768e8335ad4c0297bb66
fogadOg <fogadm06@gmail.com>
2024-08-19 10:59:21 +0200

    fixing formatting

--- app/components/admin/adminControls/index.tsx
+++ app/components/admin/adminControls/index.tsx
@@ -2,36 +2,63 @@ import { useState } from "react";
 import { Modal } from "../../modal";
 import { PermissionControls } from "./permissionControls";
 import { InviteUsersForm } from "./inviteUsersForm";
-import { PeopleIcon } from "@/app/assets/peopleIcon";
-import { InviteIcon } from "@/app/assets/inviteIcon";
-import { CameraManagement } from "./cameraManagement";
-import { IngressIcon } from "@/app/assets/ingressIcon";
-import { PauseButton } from "./pauseButton";
+import { IngressManagement } from "./ingressManagement";
 
 export const AdminControls = ({ token }: { token: string }) => {
+  const [selectedTab, setSelectedTab] = useState("tab1");
 
   return (
-      <div role="tablist" className="tabs tabs-lifted">
-        <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Manage ingress" checked/>
-        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-          <CameraManagement token={token} />
-        </div>
-
-        <input
-          type="radio"
-          name="my_tabs_2"
-          role="tab"
-          className="tab"
-          aria-label="Manage ingress"
-          />
-        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-          <PermissionControls token={token} />
-        </div>
-
-        <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Invite users" />
-        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-          <InviteUsersForm token={token} />
-        </div>
+    <div role="tablist" className="tabs tabs-lifted">
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab"
+        aria-label="Manage ingress"
+        checked={selectedTab === "tab1"}
+        onChange={() => setSelectedTab("tab1")}
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+        hidden={selectedTab !== "tab1"}
+      >
+        <IngressManagement token={token} />
       </div>
+
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab"
+        aria-label="Manage permissions"
+        checked={selectedTab === "tab2"}
+        onChange={() => setSelectedTab("tab2")}
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+        hidden={selectedTab !== "tab2"}
+      >
+        <PermissionControls token={token} />
+      </div>
+
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab"
+        aria-label="Invite users"
+        checked={selectedTab === "tab3"}
+        onChange={() => setSelectedTab("tab3")}
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+        hidden={selectedTab !== "tab3"}
+      >
+        <InviteUsersForm token={token} />
+      </div>
+    </div>
   );
 };
