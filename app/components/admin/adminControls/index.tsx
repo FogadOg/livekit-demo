d0cd9ff441876201f623b514e5bcdc5d618e2629
fogadOg <fogadm06@gmail.com>
2024-08-19 09:15:16 +0200

    changing from models to tabs

--- app/components/admin/adminControls/index.tsx
+++ app/components/admin/adminControls/index.tsx
@@ -10,31 +10,27 @@ import { PauseButton } from "./pauseButton";
 export const AdminControls = ({ token }: { token: string }) => {
 
   return (
-    <>
-      <Modal
-        title="Manage ingress"
-        content={<CameraManagement token={token} />}
-        buttonText={"Manage ingress"}
-        modelName="ingressManager"
-        icon={<IngressIcon />}
-      />
-      <Modal
-        title="Users permissions"
-        content={<PermissionControls token={token} />}
-        buttonText="Users permissions"
-        modelName="PermissionControls"
-        icon={<PeopleIcon />}
-      />
+      <div role="tablist" className="tabs tabs-lifted">
+        <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Manage ingress" checked/>
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+          <CameraManagement token={token} />
+        </div>
 
-      {/* Invite users */}
-      <Modal
-        title="Permissions of invite link"
-        content={<InviteUsersForm token={token} />}
-        buttonText={"Invite users"}
-        modelName="InviteUsers"
-        icon={<InviteIcon />}
-      />
-      <PauseButton/>
-    </>
+        <input
+          type="radio"
+          name="my_tabs_2"
+          role="tab"
+          className="tab"
+          aria-label="Manage ingress"
+          />
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+          <PermissionControls token={token} />
+        </div>
+
+        <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Invite users" />
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+          <InviteUsersForm token={token} />
+        </div>
+      </div>
   );
 };
