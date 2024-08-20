1c9df6e4092b74d204ea258d7dcd64b520574b79
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-20 15:44:14 +0200

    Speaker layout defaulting to first user

--- app/room/components/layouts/SpeakerLayout.tsx
+++ app/room/components/layouts/SpeakerLayout.tsx
@@ -1,4 +1,4 @@
-import { TrackReference } from '@livekit/components-core';
+import { TrackReference } from "@livekit/components-core";
 import {
   CarouselLayout,
   Chat,
@@ -10,36 +10,40 @@ import {
   useLocalParticipantPermissions,
   useRoomInfo,
   useVisualStableUpdate,
-} from '@livekit/components-react';
-import { TranscriptionButton } from '../transcription/transcriptionButton';
-import Caption from '../transcription/caption';
-import { CustomAudioRenderer } from '../../../components/customAudioRenderer';
-import { useEffect, useState } from 'react';
-import { CustomGridLayout } from './customGridLayout';
-import useTracksFilter from '@/app/util/useTracksFilter';
-import RecordVoiceOverIcon from '@mui/icons-material/RecordVoiceOver';
+} from "@livekit/components-react";
+import { TranscriptionButton } from "../transcription/transcriptionButton";
+import Caption from "../transcription/caption";
+import { CustomAudioRenderer } from "../../../components/customAudioRenderer";
+import { useEffect, useState } from "react";
+import { CustomGridLayout } from "./customGridLayout";
+import useTracksFilter from "@/app/util/useTracksFilter";
+import RecordVoiceOverIcon from "@mui/icons-material/RecordVoiceOver";
 
 interface LayoutProps {
   tracks: TrackReferenceOrPlaceholder[];
 }
 
 const SpeakerLayout = ({ tracks: references }: LayoutProps) => {
-
   const filteredTracks = useTracksFilter(references);
-  const [remainingTracks, setRemainingTracks] = useState(filteredTracks)
+  const [remainingTracks, setRemainingTracks] = useState(filteredTracks);
 
+  const mainTrack =
+    filteredTracks.find((track) => track.participant.isSpeaking) ||
+    filteredTracks[0];
 
-  const mainTrack = filteredTracks.find((track) => track.participant.isSpeaking)
-
-  const [lastSpoken, setLastSpoken] = useState(mainTrack!)
+  const [lastSpoken, setLastSpoken] = useState(mainTrack!);
 
   useEffect(() => {
-    if(mainTrack){
-      setLastSpoken(mainTrack)
-      setRemainingTracks(filteredTracks.filter((track) => track.participant.identity !== mainTrack.participant.identity))
+    if (mainTrack) {
+      setLastSpoken(mainTrack);
+      setRemainingTracks(
+        filteredTracks.filter(
+          (track) =>
+            track.participant.identity !== mainTrack.participant.identity
+        )
+      );
     }
-  }, [filteredTracks])
-  
+  }, [filteredTracks]);
 
   const agentPresent = filteredTracks.length !== references.length;
 
@@ -53,41 +57,57 @@ const SpeakerLayout = ({ tracks: references }: LayoutProps) => {
     } catch {
       return false;
     }
-  })()
-
+  })();
 
   if (!lastSpoken) {
-    return <CustomGridLayout/>;
+    return <CustomGridLayout />;
   } else if (remainingTracks.length === 0) {
-    return <CustomGridLayout/>;
+    return <CustomGridLayout />;
   }
-  
+
   return (
-    <div className="lk-focus-layout" style={{ height: "calc(100vh - var(--lk-control-bar-height))" }}>
-      <CarouselLayout tracks={remainingTracks.filter((track)=>track.participant.identity !== mainTrack?.participant.identity)}>
+    <div
+      className="lk-focus-layout"
+      style={{ height: "calc(100vh - var(--lk-control-bar-height))" }}
+    >
+      <CarouselLayout
+        tracks={remainingTracks.filter(
+          (track) =>
+            track.participant.identity !== mainTrack?.participant.identity
+        )}
+      >
         <div className="relative">
           <ParticipantTile className="h-full" />
           <div className="absolute top-5 left-5">
-            {transcriptAvailable && <TranscriptionButton icon={<RecordVoiceOverIcon/>}/>}
+            {transcriptAvailable && (
+              <TranscriptionButton icon={<RecordVoiceOverIcon />} />
+            )}
           </div>
           <div className="absolute top-[75%] origin-top left-[2%] max-w-[96%] xl:top-[80%] xl:left-[20%] xl:max-w-[65%]">
             {/* Caption visible if agent present*/}
-            {agentPresent && <Caption/>}
+            {agentPresent && <Caption />}
           </div>
         </div>
       </CarouselLayout>
 
       <div className="relative">
-          <ParticipantTile className="h-full" trackRef={lastSpoken as TrackReference}/>
-          <div className="absolute top-10 left-20">
-            {transcriptAvailable && <TranscriptionButton trackRef={lastSpoken as TrackReference} hasButtonText={true}/>}
-          </div>
-          <div className="absolute top-[75%] origin-top left-[2%] max-w-[96%] xl:top-[80%] xl:left-[20%] xl:max-w-[65%]">
-            {agentPresent && <Caption trackRef={lastSpoken as TrackReference}/>}
-          </div>
+        <ParticipantTile
+          className="h-full"
+          trackRef={lastSpoken as TrackReference}
+        />
+        <div className="absolute top-10 left-20">
+          {transcriptAvailable && (
+            <TranscriptionButton
+              trackRef={lastSpoken as TrackReference}
+              hasButtonText={true}
+            />
+          )}
         </div>
-      <CustomAudioRenderer/>
-
+        <div className="absolute top-[75%] origin-top left-[2%] max-w-[96%] xl:top-[80%] xl:left-[20%] xl:max-w-[65%]">
+          {agentPresent && <Caption trackRef={lastSpoken as TrackReference} />}
+        </div>
+      </div>
+      <CustomAudioRenderer />
     </div>
   );
 };
