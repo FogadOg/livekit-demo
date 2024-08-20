7a8f804203933de59079d4b9d3e54546769ddb1e
fogadOg <fogadm06@gmail.com>
2024-08-20 10:28:50 +0200

    implementing custom focus layer

--- app/room/components/layouts/SpeakerLayout.tsx
+++ app/room/components/layouts/SpeakerLayout.tsx
@@ -1,71 +1,71 @@
-import { TrackReference } from '@livekit/components-core';
+import { TrackReferenceOrPlaceholder, getTrackReferenceId } from '@livekit/components-core';
 import {
+  AudioTrack as LiveKitAudioTrack,
   CarouselLayout,
-  Chat,
   FocusLayout,
-  GridLayout,
   ParticipantTile,
-  TrackReferenceOrPlaceholder,
-  VideoTrack,
   useLocalParticipantPermissions,
   useRoomInfo,
-  useVisualStableUpdate,
+  AudioTrack,
 } from '@livekit/components-react';
+import { TrackReference } from "@livekit/components-react";
 import { TranscriptionButton } from '../transcription/transcriptionButton';
 import Caption from '../transcription/caption';
 import { CustomAudioRenderer } from '../../../components/customAudioRenderer';
 import tracksFilter from '../../../util/tracksFilter';
 import { useEffect, useState } from 'react';
 import { CustomGridLayout } from './customGridLayout';
-
+import { CustomFocusLayout } from './customFocuseLayout';
 
 interface LayoutProps {
   tracks: TrackReferenceOrPlaceholder[];
 }
 
 const SpeakerLayout = ({ tracks: references }: LayoutProps) => {
-
   const filteredTracks = tracksFilter(references);
-  const [remainingTracks, setRemainingTracks] = useState(filteredTracks)
+  const [remainingTracks, setRemainingTracks] = useState(filteredTracks);
 
+  const mainTrack = filteredTracks.find((track) => track.participant.isSpeaking);
 
-  const mainTrack = filteredTracks.find((track) => track.participant.isSpeaking)
-
-  const [lastSpoken, setLastSpoken] = useState(mainTrack!)
+  const [lastSpoken, setLastSpoken] = useState(mainTrack!);
 
   useEffect(() => {
-    if(mainTrack){
-      setLastSpoken(mainTrack)
-      setRemainingTracks(filteredTracks.filter((track) => track.participant.identity !== mainTrack.participant.identity))
+    if (mainTrack) {
+      setLastSpoken(mainTrack);
+      setRemainingTracks(filteredTracks.filter((track) => track.participant.identity !== mainTrack.participant.identity));
     }
-  }, [filteredTracks])
-  
+  }, [filteredTracks]);
 
   const agentPresent = filteredTracks.length !== references.length;
-
   const participantPermissions = useLocalParticipantPermissions();
-
   const roomInfo = useRoomInfo();
 
   const transcriptAvailable = (() => {
     try {
-      return !!JSON.parse(roomInfo.metadata || "{}").transcript;
+      return !!JSON.parse(roomInfo.metadata || '{}').transcript;
     } catch {
       return false;
     }
-  })
+  })();
 
 
-  if (!lastSpoken) {    
-    return <CustomGridLayout/>;
-  }
-  
+
   return (
-    <div className="lk-focus-layout" style={{ height: "calc(100vh - var(--lk-control-bar-height))" }}>
-      <CarouselLayout tracks={remainingTracks.filter((track)=>track.participant.identity !== mainTrack?.participant.identity)}>
-        <ParticipantTile />
+    <div className="lk-focus-layout" style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
+      <CarouselLayout tracks={remainingTracks.filter((track) => track.participant.identity !== mainTrack?.participant.identity)}>
+        <div className="relative">
+          <ParticipantTile className="h-full" />
+          <div className="absolute top-10 left-20">
+            {transcriptAvailable && <TranscriptionButton />}
+          </div>
+          <div className="absolute top-[75%] origin-top left-[2%] max-w-[96%] xl:top-[80%] xl:left-[20%] xl:max-w-[65%]">
+            {agentPresent && <Caption />}
+          </div>
+        </div>
       </CarouselLayout>
-      <FocusLayout trackRef={lastSpoken as TrackReference} />
+      <CustomFocusLayout track={lastSpoken} />
+
+      <CustomAudioRenderer />
     </div>
   );
 };
