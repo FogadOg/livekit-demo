6487c4fa0dcdca06b7e7e3a3b1d1491320ae4b18
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-20 14:20:15 +0200

    Need to check if localstorage is available

--- app/hooks/useToken.ts
+++ app/hooks/useToken.ts
@@ -1,14 +1,22 @@
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function useToken() {
   const searchParams = useSearchParams();
 
   const permissionsToken = searchParams.get("permissionsToken");
   const roomId = searchParams.get("roomId");
-  const adminToken = localStorage.getItem("roomAdmin-" + roomId);
 
-  const [token, setToken] = useState(adminToken ? adminToken : "");
+  const [token, setToken] = useState("");
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // This code will only run in the browser
+      const adminToken = localStorage.getItem("roomAdmin-" + roomId);
+      if (adminToken) {
+        setToken(adminToken);
+      }
+    }
+  }, [roomId, permissionsToken]);
 
   return { token, setToken, permissionsToken };
 }
