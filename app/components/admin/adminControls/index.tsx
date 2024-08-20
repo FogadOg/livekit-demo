04ddc78e13873718494adca65e19282f584cdda2
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-20 16:11:45 +0200

    Fixed tabs going inside panel

--- app/components/admin/adminControls/index.tsx
+++ app/components/admin/adminControls/index.tsx
@@ -5,35 +5,64 @@ import { Layouts } from "./layouts";
 import { LayoutChange } from "./layoutChange";
 
 export const AdminControls = ({ token }: { token: string }) => {
-
   return (
-      <div role="tablist" className="tabs tabs-lifted">
-        <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Manage ingress" defaultChecked/>
-        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-          <IngressManagement token={token} />
-        </div>
-
-        <input
-          type="radio"
-          name="my_tabs_2"
-          role="tab"
-          className="tab"
-          aria-label="Manage users"
-          />
-        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-          <PermissionControls token={token} />
-        </div>
-
-        <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Invite users" />
-        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-          <InviteUsersForm token={token} />
-        </div>
-
-        <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Change layout" />
-        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-          <LayoutChange/>
-        </div>
-        
+    <div role="tablist" className="tabs tabs-lifted">
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab h-14"
+        aria-label="Manage ingress"
+        defaultChecked
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+      >
+        <IngressManagement token={token} />
       </div>
+
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab h-14"
+        aria-label="Manage users"
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+      >
+        <PermissionControls token={token} />
+      </div>
+
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab h-14"
+        aria-label="Invite users"
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+      >
+        <InviteUsersForm token={token} />
+      </div>
+
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab h-14"
+        aria-label="Change layout"
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+      >
+        <LayoutChange />
+      </div>
+    </div>
   );
 };
