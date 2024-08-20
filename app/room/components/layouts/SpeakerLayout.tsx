e1b466493c89e065834138f9c10ecd551eef7efe
fogadOg <fogadm06@gmail.com>
2024-08-20 11:12:28 +0200

    fixing caption for focus participant

--- app/room/components/layouts/SpeakerLayout.tsx
+++ app/room/components/layouts/SpeakerLayout.tsx
@@ -1,71 +1,94 @@
-import { TrackReferenceOrPlaceholder, getTrackReferenceId } from '@livekit/components-core';
+import { TrackReference } from '@livekit/components-core';
 import {
-  AudioTrack as LiveKitAudioTrack,
-  CarouselLayout,
-  FocusLayout,
-  ParticipantTile,
-  useLocalParticipantPermissions,
-  useRoomInfo,
   AudioTrack,
+  CarouselLayout,
+  Chat,
+  FocusLayout,
+  GridLayout,
+  ParticipantTile,
+  TrackReferenceOrPlaceholder,
+  VideoTrack,
+  useLocalParticipantPermissions,
+  useParticipants,
+  useRoomInfo,
+  useVisualStableUpdate,
 } from '@livekit/components-react';
-import { TrackReference } from "@livekit/components-react";
+import { getTrackReferenceId } from "@livekit/components-core";
 import { TranscriptionButton } from '../transcription/transcriptionButton';
 import Caption from '../transcription/caption';
 import { CustomAudioRenderer } from '../../../components/customAudioRenderer';
 import tracksFilter from '../../../util/tracksFilter';
 import { useEffect, useState } from 'react';
 import { CustomGridLayout } from './customGridLayout';
-import { CustomFocusLayout } from './customFocuseLayout';
+
 
 interface LayoutProps {
   tracks: TrackReferenceOrPlaceholder[];
 }
 
 const SpeakerLayout = ({ tracks: references }: LayoutProps) => {
+
   const filteredTracks = tracksFilter(references);
-  const [remainingTracks, setRemainingTracks] = useState(filteredTracks);
+  const [remainingTracks, setRemainingTracks] = useState(filteredTracks)
 
-  const mainTrack = filteredTracks.find((track) => track.participant.isSpeaking);
 
-  const [lastSpoken, setLastSpoken] = useState(mainTrack!);
+  const mainTrack = filteredTracks.find((track) => track.participant.isSpeaking)
+
+  const [lastSpoken, setLastSpoken] = useState(mainTrack!)
 
   useEffect(() => {
-    if (mainTrack) {
-      setLastSpoken(mainTrack);
-      setRemainingTracks(filteredTracks.filter((track) => track.participant.identity !== mainTrack.participant.identity));
+    if(mainTrack){
+      setLastSpoken(mainTrack)
+      setRemainingTracks(filteredTracks.filter((track) => track.participant.identity !== mainTrack.participant.identity))
     }
-  }, [filteredTracks]);
+  }, [filteredTracks])
+  
 
   const agentPresent = filteredTracks.length !== references.length;
+
   const participantPermissions = useLocalParticipantPermissions();
+
   const roomInfo = useRoomInfo();
 
   const transcriptAvailable = (() => {
     try {
-      return !!JSON.parse(roomInfo.metadata || '{}').transcript;
+      return !!JSON.parse(roomInfo.metadata || "{}").transcript;
     } catch {
       return false;
     }
-  })();
+  })()
 
 
-
+  if (!lastSpoken) {    
+    return <CustomGridLayout/>;
+  }
+  
   return (
-    <div className="lk-focus-layout" style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
-      <CarouselLayout tracks={remainingTracks.filter((track) => track.participant.identity !== mainTrack?.participant.identity)}>
+    <div className="lk-focus-layout" style={{ height: "calc(100vh - var(--lk-control-bar-height))" }}>
+      <CarouselLayout tracks={remainingTracks.filter((track)=>track.participant.identity !== mainTrack?.participant.identity)}>
         <div className="relative">
           <ParticipantTile className="h-full" />
           <div className="absolute top-10 left-20">
-            {transcriptAvailable && <TranscriptionButton />}
+            {transcriptAvailable && <TranscriptionButton/>}
           </div>
           <div className="absolute top-[75%] origin-top left-[2%] max-w-[96%] xl:top-[80%] xl:left-[20%] xl:max-w-[65%]">
-            {agentPresent && <Caption />}
+            {/* Caption visible if agent present*/}
+            {agentPresent && <Caption/>}
           </div>
         </div>
       </CarouselLayout>
-      <CustomFocusLayout track={lastSpoken} />
 
-      <CustomAudioRenderer />
+      <div className="relative">
+          <ParticipantTile className="h-full" trackRef={lastSpoken as TrackReference}/>
+          <div className="absolute top-10 left-20">
+            {transcriptAvailable && <TranscriptionButton trackRef={lastSpoken as TrackReference}/>}
+          </div>
+          <div className="absolute top-[75%] origin-top left-[2%] max-w-[96%] xl:top-[80%] xl:left-[20%] xl:max-w-[65%]">
+            {agentPresent && <Caption trackRef={lastSpoken as TrackReference}/>}
+          </div>
+        </div>
+      <CustomAudioRenderer/>
+
     </div>
   );
 };
