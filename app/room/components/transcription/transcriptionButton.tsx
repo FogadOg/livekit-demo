7483111f38581ab43efe279f5846637288901ed3
fogadOg <fogadm06@gmail.com>
2024-08-20 13:45:09 +0200

    adding new parameter

--- app/room/components/transcription/transcriptionButton.tsx
+++ app/room/components/transcription/transcriptionButton.tsx
@@ -4,20 +4,24 @@ import {
   useEnsureTrackRef,
 } from "@livekit/components-react";
 import { TranscriptTile } from "./transcriptionTile";
+import RecordVoiceOverIcon from '@mui/icons-material/RecordVoiceOver';
 
 interface TranscriptionButtonProps {
+  hasButtonText?: boolean
+  icon?: JSX.Element
   trackRef?: TrackReferenceOrPlaceholder;
 }
 
-export const TranscriptionButton = ({ trackRef }: TranscriptionButtonProps) => {
+export const TranscriptionButton = ({ trackRef, icon, hasButtonText = false }: TranscriptionButtonProps) => {
   const trackReference = useEnsureTrackRef(trackRef);
   const participant = trackReference.participant;
   return (
     <Modal
       title={`${participant.name || participant.identity}s transcription`}
       content={<TranscriptTile participant={participant} />}
-      buttonText={`${participant.name || participant.identity}'s transcript`}
+      buttonText={hasButtonText ? `${participant.name || participant.identity}'s transcript`:""}
       modelName={`${participant.identity}s transcription`}
+      icon={<RecordVoiceOverIcon/>}
     />
   );
 };
