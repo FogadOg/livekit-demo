cb0332195ab88d9200e0d78749cc6e344d0cbd25
fogadOg <fogadm06@gmail.com>
2024-08-20 13:50:00 +0200

    fixing error

--- app/room/components/layouts/SpeakerLayout.tsx
+++ app/room/components/layouts/SpeakerLayout.tsx
@@ -1,4 +1,4 @@
-import { TrackReference } from "@livekit/components-core";
+import { TrackReference } from '@livekit/components-core';
 import {
   AudioTrack,
   CarouselLayout,
@@ -13,39 +13,36 @@ import {
   useRoomInfo,
   useVisualStableUpdate,
 } from '@livekit/components-react';
+import { getTrackReferenceId } from "@livekit/components-core";
 import { TranscriptionButton } from '../transcription/transcriptionButton';
 import Caption from '../transcription/caption';
 import { CustomAudioRenderer } from '../../../components/customAudioRenderer';
-import tracksFilter from '../../../util/tracksFilter';
 import { useEffect, useState } from 'react';
 import { CustomGridLayout } from './customGridLayout';
-
+import RecordVoiceOverIcon from '@mui/icons-material/RecordVoiceOver';
+import useTracksFilter from '@/app/util/useTracksFilter';
 
 interface LayoutProps {
   tracks: TrackReferenceOrPlaceholder[];
 }
 
 const SpeakerLayout = ({ tracks: references }: LayoutProps) => {
+
   const filteredTracks = useTracksFilter(references);
-  const [remainingTracks, setRemainingTracks] = useState(filteredTracks);
+  const [remainingTracks, setRemainingTracks] = useState(filteredTracks)
 
-  const mainTrack = filteredTracks.find(
-    (track) => track.participant.isSpeaking
-  );
 
-  const [lastSpoken, setLastSpoken] = useState(mainTrack!);
+  const mainTrack = filteredTracks.find((track) => track.participant.isSpeaking)
+
+  const [lastSpoken, setLastSpoken] = useState(mainTrack!)
 
   useEffect(() => {
-    if (mainTrack) {
-      setLastSpoken(mainTrack);
-      setRemainingTracks(
-        filteredTracks.filter(
-          (track) =>
-            track.participant.identity !== mainTrack.participant.identity
-        )
-      );
+    if(mainTrack){
+      setLastSpoken(mainTrack)
+      setRemainingTracks(filteredTracks.filter((track) => track.participant.identity !== mainTrack.participant.identity))
     }
-  }, [filteredTracks, mainTrack]);
+  }, [filteredTracks])
+  
 
   const agentPresent = filteredTracks.length !== references.length;
 
@@ -53,26 +50,32 @@ const SpeakerLayout = ({ tracks: references }: LayoutProps) => {
 
   const roomInfo = useRoomInfo();
 
-  const transcriptAvailable = () => {
+  const transcriptAvailable = (() => {
     try {
       return !!JSON.parse(roomInfo.metadata || "{}").transcript;
     } catch {
       return false;
     }
-  })
+  })()
 
 
-  if (!lastSpoken) {
+  if (!lastSpoken) {    
     return <CustomGridLayout/>;
-  } else if (remainingTracks.length === 0) {
-    const trackRef = lastSpoken as TrackReference;
-    return <VideoTrack trackRef={trackRef} />;
   }
-
+  
   return (
     <div className="lk-focus-layout" style={{ height: "calc(100vh - var(--lk-control-bar-height))" }}>
       <CarouselLayout tracks={remainingTracks.filter((track)=>track.participant.identity !== mainTrack?.participant.identity)}>
-        <ParticipantTile />
+        <div className="relative">
+          <ParticipantTile className="h-full" />
+          <div className="absolute top-5 left-5">
+            {transcriptAvailable && <TranscriptionButton icon={<RecordVoiceOverIcon/>}/>}
+          </div>
+          <div className="absolute top-[75%] origin-top left-[2%] max-w-[96%] xl:top-[80%] xl:left-[20%] xl:max-w-[65%]">
+            {/* Caption visible if agent present*/}
+            {agentPresent && <Caption/>}
+          </div>
+        </div>
       </CarouselLayout>
 
       <div className="relative">
