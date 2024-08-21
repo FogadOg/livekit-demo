9134c891e37283a48a9a1c895257ae4af02edc12
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-21 16:05:57 +0200

    pause button updating on metadata change

--- app/components/admin/adminControls/pauseButton.tsx
+++ app/components/admin/adminControls/pauseButton.tsx
@@ -1,6 +1,6 @@
 import { addMetadataToRoom } from "@/app/actions/metadataAction";
 import { useRoomInfo } from "@livekit/components-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PauseIcon from "@mui/icons-material/Pause";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import { parseMetadata } from "@/app/util/parseMetadata";
@@ -8,8 +8,13 @@ import { parseMetadata } from "@/app/util/parseMetadata";
 export const PauseButton = () => {
   const roomInfo = useRoomInfo();
 
-  const pause = !!parseMetadata(roomInfo.metadata!).pause;
-  const [paused, setPaused] = useState(pause);
+  const initialPause = !!parseMetadata(roomInfo.metadata!).pause;
+  const [paused, setPaused] = useState(initialPause);
+
+  useEffect(() => {
+    const newPause = !!parseMetadata(roomInfo.metadata!).pause;
+    setPaused(newPause);
+  }, [roomInfo.metadata]);
 
   async function handlePause() {
     addMetadataToRoom(roomInfo.name, "pause", !paused);
