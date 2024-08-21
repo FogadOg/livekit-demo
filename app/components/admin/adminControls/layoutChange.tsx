1284797c28acf89a29d045b6265876f782c69e20
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-21 14:45:53 +0200

    Room actions

--- app/components/admin/adminControls/layoutChange.tsx
+++ app/components/admin/adminControls/layoutChange.tsx
@@ -1,29 +1,24 @@
-import { addMetadataToRoom } from "@/app/actions/metadataAction"
-import { useRoomInfo } from "@livekit/components-react"
-import ViewModuleIcon from '@mui/icons-material/ViewModule';
-import ViewSidebarIcon from '@mui/icons-material/ViewSidebar';
-
+import { addMetadataToRoom } from "@/app/actions/metadataAction";
+import { useRoomInfo } from "@livekit/components-react";
+import ViewModuleIcon from "@mui/icons-material/ViewModule";
+import ViewSidebarIcon from "@mui/icons-material/ViewSidebar";
 
 export const LayoutChange = () => {
-    const roomInfo = useRoomInfo()
-    function changeToGridLayout() {
-        addMetadataToRoom(roomInfo.name, "layout", "grid")
-        console.log(roomInfo.metadata);
-    }
-    function changeToSpeakerLayout() {
-        addMetadataToRoom(roomInfo.name, "layout", "speaker")
-        console.log(roomInfo.metadata);
-        
-    }
-    return (
-        <div>
-            <button className="btn lk-button" onClick={changeToGridLayout}>
-                <ViewModuleIcon/>
-            </button>
-            <button className="btn lk-button" onClick={changeToSpeakerLayout}>
-                <ViewSidebarIcon/>
-            </button>
-        </div>
-    )
-}
-
+  const roomInfo = useRoomInfo();
+  function changeToGridLayout() {
+    addMetadataToRoom(roomInfo.name, "layout", "grid");
+  }
+  function changeToSpeakerLayout() {
+    addMetadataToRoom(roomInfo.name, "layout", "speaker");
+  }
+  return (
+    <>
+      <button className="btn lk-button" onClick={changeToGridLayout}>
+        <ViewModuleIcon />
+      </button>
+      <button className="btn lk-button" onClick={changeToSpeakerLayout}>
+        <ViewSidebarIcon />
+      </button>
+    </>
+  );
+};
