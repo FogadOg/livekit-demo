8952420babc3d6967de38e2367a9549ad11aa13f
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-22 08:51:54 +0200

    Keeping aspect ratio on ingress

--- app/room/components/layouts/customParticipantTile.tsx
+++ app/room/components/layouts/customParticipantTile.tsx
@@ -1,24 +1,33 @@
 "use client";
 
 import React from "react";
-import { ParticipantTile, useEnsureTrackRef } from "@livekit/components-react";
+import {
+  ParticipantTile,
+  TrackReferenceOrPlaceholder,
+  useEnsureTrackRef,
+} from "@livekit/components-react";
 import "@livekit/components-styles";
 import { TranscriptionButton } from "../transcription/transcriptionButton";
 import Caption from "../transcription/caption";
+import { ParticipantKind } from "livekit-client";
 
 interface CustomParticipantTileProps {
   transcriptAvailable: boolean;
   agentPresent: boolean;
   transcriptButtonText?: boolean;
+  trackRef?: TrackReferenceOrPlaceholder;
 }
 export const CustomParticipantTile = ({
   transcriptAvailable,
   agentPresent,
   transcriptButtonText = false,
+  trackRef,
 }: CustomParticipantTileProps) => {
+  const trackReference = useEnsureTrackRef(trackRef);
+  const isIngress = trackReference.participant.kind === ParticipantKind.INGRESS;
   return (
     <div className="relative">
-      <ParticipantTile className="h-full" />
+      <ParticipantTile className={"h-full " + (isIngress ? "ingress" : "")} />
       <div className="absolute top-5 left-5">
         {transcriptAvailable && (
           <TranscriptionButton hasButtonText={transcriptButtonText} />
